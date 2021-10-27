bfac0d7dd3c01307b342a8351838ca2bce4577e5
tarilabs <matteo.mortari@gmail.com>
2021-10-27 19:53:37 +0200

    Scripts for version update adapted from OP-web

---
+++ scripts/rsync_website.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#############################################################################################################################################################################
+#
+#   You can't execute this script locally since keys, stored in Jenkins, are needed
+#   1. you need to store the id_rsa and known_host file on your local
+#   (for these pwd ask mbiarnes or geoffrey)
+#   2. create a optaplanner directory underneath $HOME/.ssh to store the needed keys ($HOME/.ssh/optaplanner)
+#   3. copy known_hosts and id_rsa (the same as on Jenkins) to $HOME/.ssh/optaplanner
+#   Then you should use:
+#   rsync --dry-run -Pavqr -e "ssh -i $HOME/.ssh/optaplanner/id_rsa" --protocol=28 --delete-after target/website/* optaplanner@filemgmt.jboss.org:www_htdocs/optaplanner
+#   the --dry-run is for testing so you can see if anything is missing. The files are not synced! If you want to execute a "real" rsync please remove --dry-run
+#
+#############################################################################################################################################################################
+
+rsync -Pavqr --protocol=28 --delete-after target/website/* drools@filemgmt.jboss.org:www_htdocs/drools
---
+++ scripts/update-versions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function display_help() {
+  readonly script_name="./$(basename "$0")"
+
+  echo "This script updates versions and the 'releaseDate' in pom.yml during a release."
+  echo "Use for standard final releases only, as it does not distinguish between 'latest' and 'latestFinal' versions."
+  echo
+  echo "Usage:"
+  echo "  $script_name NEW_FINAL_VERSION NEW_SNAPSHOT_VERSION"
+  echo "  $script_name --help"
+}
+
+if [[ $1 == "--help" ]]; then
+  display_help
+  exit 0
+fi
+
+if [[ $# -ne 2 ]]; then
+  echo "Illegal number of arguments."
+  display_help
+  exit 1
+fi
+
+readonly new_release=$1
+readonly new_snapshot=$2
+
+readonly release_date=$(date +%Y-%m-%d)
+
+this_script_directory="${BASH_SOURCE%/*}"
+if [[ ! -d "$this_script_directory" ]]; then
+  this_script_directory="$PWD"
+fi
+readonly pom_yml_file="$this_script_directory/../data/pom.yml" # locate jbake pom.yml value file
+
+sed -i -E "s/releaseDate: [0-9]+-[0-9]+-[0-9]+/releaseDate: $release_date/g" "$pom_yml_file"
+sed -i -E "s/7\.[0-9]+\.[0-9]+\.(Final|Beta[0-9]*|CR[0-9]*)/$new_release/g" "$pom_yml_file" # change only 7.x series for Drools
+sed -i -E "s/7\.[0-9]+\.[0-9]+\-SNAPSHOT/$new_snapshot/g" "$pom_yml_file" # change only 7.x series for Drools
+
+# Update antora-playbook.yml to point to the latest release branch of https://github.com/kiegroup/<project>.
+# if [[ "$new_release" == *Final* ]]; then
+#   readonly antora_playbook_yml_file="$this_script_directory/../optaplanner-website-docs/antora-playbook.yml"
+#   readonly version_array=(${new_release//./ })
+#   readonly release_branch="${version_array[0]}.${version_array[1]}.x"
+#   sed -i -E "s/branches: \[[0-9]+\.[0-9]+\.x\]/branches: [$release_branch]/" "$antora_playbook_yml_file"
+# fi
