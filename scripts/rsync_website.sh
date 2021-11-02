d48e8854eabbaf8815e153573ad50a0e36b302f6
Matteo Mortari <matteo.mortari@gmail.com>
2021-11-02 09:54:11 +0100

    Update versions scripts (#177)
    * Scripts for version update adapted from OP-web
    
    * Drools6 `releaseDateV6` with trailing `V6`
    
    * latestDroolsJbpmToolsZip inline fixed value in content
    
    * code review comment

---
+++ scripts/rsync_website.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#############################################################################################################################################################################
+#
+#   You can't execute this script locally since keys, stored in Jenkins, are needed
+#   1. you need to store the id_rsa and known_host file on your local
+#   (for these pwd ask mbiarnes or geoffrey)
+#   2. create a drools directory underneath $HOME/.ssh to store the needed keys ($HOME/.ssh/drools)
+#   3. copy known_hosts and id_rsa (the same as on Jenkins) to $HOME/.ssh/drools
+#   Then you should use:
+#   rsync --dry-run -Pavqr -e "ssh -i $HOME/.ssh/drools/id_rsa" --protocol=28 --delete-after target/website/* drools@filemgmt.jboss.org:www_htdocs/drools
+#   the --dry-run is for testing so you can see if anything is missing. The files are not synced! If you want to execute a "real" rsync please remove --dry-run
+#
+#############################################################################################################################################################################
+
+rsync -Pavqr --protocol=28 --delete-after target/website/* drools@filemgmt.jboss.org:www_htdocs/drools
