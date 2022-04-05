51b8d18fbb20227f7ab8013c34ad08a4ad9edcad
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2022-04-05 17:00:41 +0200

    change of filemgmt.jboss.org server for publishing website (#206)

--- scripts/rsync_website.sh
+++ scripts/rsync_website.sh
@@ -7,9 +7,9 @@
 #   2. create a drools directory underneath $HOME/.ssh to store the needed keys ($HOME/.ssh/drools)
 #   3. copy known_hosts and id_rsa (the same as on Jenkins) to $HOME/.ssh/drools
 #   Then you should use:
-#   rsync --dry-run -Pavqr -e "ssh -i $HOME/.ssh/drools/id_rsa" --protocol=28 --delete-after target/website/* drools@filemgmt.jboss.org:www_htdocs/drools
+#   rsync --dry-run -Pavqr -e "ssh -i $HOME/.ssh/drools/id_rsa" --protocol=28 --delete-after target/website/* drools@filemgmt-prod-sync.jboss.org:www_htdocs/drools
 #   the --dry-run is for testing so you can see if anything is missing. The files are not synced! If you want to execute a "real" rsync please remove --dry-run
 #
 #############################################################################################################################################################################
 
-rsync -Pavqr --protocol=28 --delete-after target/website/* drools@filemgmt.jboss.org:www_htdocs/drools
+rsync -Pavqr -p2222 --protocol=28 --delete-after target/website/* drools@filemgmt-prod-sync.jboss.org:www_htdocs/drools
